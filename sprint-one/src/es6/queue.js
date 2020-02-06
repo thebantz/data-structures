acbd875f7d6941e91b3810937d8d558a383b3848
Nicole <nicoledbanta@gmail.com>
2020-02-06 14:32:25 -0800

    Completed everything except for a Functional Shared IP test

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.smallestIndex = 0;
+    this.largestIndex = 0;
+  }
+
+  enqueue(value) {
+    this.storage[this.largestIndex] = value;
+    this.largestIndex++;
+  }
+
+  dequeue() {
+    var dq = this.storage[this.smallestIndex];
+    delete this.storage[this.smallestIndex];
+    this.smallestIndex++;
+    return dq;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
+
+var stack = new stack();
