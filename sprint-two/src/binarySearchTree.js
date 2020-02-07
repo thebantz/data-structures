1a3c00df9b43b473982f2d2d11dbeb75e0e707e0
thebantz <nicoledbanta@gmail.com>
2020-02-06 21:46:31 -0800

    Attempted Linked List

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -5,3 +5,4 @@ var BinarySearchTree = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ 
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,11 +2,27 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  // list = {
+  //   head : null,
+  //     {
+  //       value: 4,
+  //       next: tail
+  //     }
+  //   tail : null
+  // }
 
   list.addToTail = function(value) {
+    // assign value to list.tail
+    list.tail = Node(value);
+    list.head = Node(value);
   };
 
   list.removeHead = function() {
+    // create a variable result and store the current node at head
+
+    // delete the node
+    // assign the list.head to currentnode.next <<--- how do we reassign the list.head
+    // return result
   };
 
   list.contains = function(target) {
